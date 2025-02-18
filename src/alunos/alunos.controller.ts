7a114d620d5aea72c884562a8951337a0b9c61b8
brenoleonel <brenoaleonels@outlook.com>
2025-02-17 21:28:15 -0300

    adicionado nova biblioteca para gerar os pdf, configurado rota no aluno no controller para funcionar e finalizado todo projeto

--- src/alunos/alunos.controller.ts
+++ src/alunos/alunos.controller.ts
@@ -8,11 +8,14 @@ import {
   Param,
   Post,
   Put,
+  Res,
   UseGuards,
 } from '@nestjs/common';
 import { AlunosService } from './alunos.service';
 import { AlunosDto } from './dto/create-alunos.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import * as PDFDocument from 'pdfkit';
+import { Response } from 'express';
 
 @UseGuards(JwtAuthGuard)
 @Controller('alunos')
@@ -90,5 +93,48 @@ export class AlunosController {
     return {
       message: 'Aluno apagado com sucesso!',
     };
+  }
+
+  @Get(':id/pdf')
+  async gerarPdf(@Param('id') id: string, @Res() res: Response) {
+    const aluno = await this.alunoService.findAlunoById(id);
+
+    if (!aluno) {
+      return res.status(404).json({ message: 'Aluno não encontrado' });
+    }
+
+    // Criar o documento PDF
+    const doc = new PDFDocument({
+      margin: 30,
+      size: 'A5',
+      layout: 'landscape', // 🔥 Define o PDF como paisagem
+    });
+    
+    // Configurar a resposta HTTP para PDF
+    res.setHeader('Content-Disposition', `attachment; filename=aluno-${id}.pdf`);
+    res.setHeader('Content-Type', 'application/pdf');
+    
+    // Pipe para enviar o PDF diretamente na resposta
+    doc.pipe(res);
+    
+    // Adicionar título
+    doc.fontSize(24).text('DADOS DO ALUNO', { align: 'center' }).moveDown(1);
+    
+    // Adicionar os dados formatados
+    doc
+      .fontSize(16)
+      .text(`ESCOLA: ${aluno.Escola}  |  CURSO: ${aluno.Curso}`, { align: 'left' })
+      .text(`NOME: ${aluno.Nome}  |  CPF: ${aluno.Cpf}`)
+      .moveDown(0.5)
+      .text(`ENDEREÇO: ${aluno.Endereco}, ${aluno.Numero}  |  BAIRRO: ${aluno.Bairro}, | CIDADE: ${aluno.Cidade}  |  ESTADO: ${aluno.Estado} `)
+      .moveDown(0.5)
+      .text(`EMAIL: ${aluno.Email} | CELULAR 1: ${aluno.Celular1}`);
+    
+    // Adicionar Celular 2 e Celular 3 apenas se existirem
+    if (aluno.Celular2) doc.text(`CELULAR 2: ${aluno.Celular2}`);
+    if (aluno.Celular3) doc.text(`CELULAR 3: ${aluno.Celular3}`);
+    
+    // Finalizar o PDF
+    doc.end();
   }
 }
